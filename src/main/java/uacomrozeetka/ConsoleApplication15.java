518e61797778fa457fc379182501d9ed39e09d07
Diana Magsumova10 <hpjava1011@gmail.com>
2022-09-07 21:24:13 +0300

    add ok

--- src/main/java/uacomrozeetka/ConsoleApplication15.java
+++ src/main/java/uacomrozeetka/ConsoleApplication15.java
@@ -1,8 +1,12 @@
 package uacomrozeetka;
 
-public class ConsoleApplication {
+public class ConsoleApplication15 {
     public static void main(String[] args) {
+        System.out.println("its ok");
+        int a = 15;
+        int b = 48;
         System.out.println("Start");
+        System.out.println("add dev a brunch");
 
     }
 }
